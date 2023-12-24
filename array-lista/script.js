e9d34e0c202a809f1cc15509671b6c1066948108
LukePadawalker <padurariu.luca@gmail.com>
2023-12-24 10:16:02 +0200

    finished main logics es 1

---
+++ array-lista/script.js
@@ -0,0 +1,29 @@
+console.log('js ok');
+
+const frigo = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'limone', 'fragola'];
+console.log(frigo);
+
+
+frigo.push("pesca");
+
+console.log(frigo);
+
+let isCocomero = false;
+
+for (i = 0; i < frigo.length && isCocomero === false; i++){
+
+    if (frigo[i] === "cocomero"){
+        isCocomero = true;
+    }
+
+
+
+}
+
+if (isCocomero = true){
+    console.log("Cocomero Trovato");
+
+}else{
+    console.log("cocomero non trovato");
+}
+
