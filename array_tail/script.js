522871498a94f269b94792637d59f10fccba020c
LukePadawalker <padurariu.luca@gmail.com>
2023-12-24 11:19:33 +0200

    finished the script es2

--- array_tail/script.js
+++ array_tail/script.js
@@ -1,19 +1,35 @@
 console.log('js ok');
 
 
-
 // Scaletta
 
 /*
 
-- creiamo un array vuoto 
 - chiediamo all'utente in numero di elementi da contenere
+- creiamo un array 
 - inseriamo nell'array il numero di elementi che l'utente ci ha chiesto
 - chiediamo all'utente il numero di elementi dell'array che dobbiamo stampare
 - stampiamo il numero degli elementi in console
 
 
 
-
 */
+
+
+const numOfElements = parseInt(prompt("Quanti elementi vuoi che contenga l'array?", "20")); 
+
+const numOfShowedElement = parseInt(prompt("Quanti elementi vuoi che stampi in console", "3")); 
+
+let array = [];
+
+for (i = 0; i < numOfElements; i++){
+
+    array.push(Math.floor(Math.random() * 100) + 1);
+
+}
+console.log(array);
+
+
+
+console.log(array.slice(-numOfShowedElement));
 
